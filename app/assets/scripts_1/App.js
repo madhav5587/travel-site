7bbbf726041ebdf36261ab956c155ce7424e293d
Venumadhav Pallerla <madhav5587@gmail.com>
2020-07-18 19:30:50 +0530

    Modified css and js files

--- app/assets/scripts_1/App.js
+++ app/assets/scripts_1/App.js
@@ -1,20 +1,12 @@
 import '../styles/styles.css'
-import Person from './modules/Person'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept()
 }
-
-/* Lesson example code below this line */
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes zero taxes.");
-  }
-}
-
-let john = new Person("John Doe", "purple");
-john.greet();
-
-let jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
